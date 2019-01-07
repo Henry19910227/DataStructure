d9d15c53d6ca31aae2b64ea8bc47e9cbc645f438
henry <toyokoyo199@gmail.com>
2019-01-08 00:49:02 +0800

    Initial Commit

---
+++ DataStructure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataStructure
+//
+//  Created by 廖冠翰 on 2019/1/8.
+//  Copyright © 2019 廖冠翰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
